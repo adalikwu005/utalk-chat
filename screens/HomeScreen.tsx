539d7375011ed9c58b22b5160c8e32c029fc418d
Michael Adalikwu <michaeladalikwu@gmail.com>
2021-12-17 18:00:16 +0100

    Init amplify and authentication implementation

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
 import { View } from "../components/Themed";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, Text, Pressable } from "react-native";
 import ChatRoomItem from "../components/ChatRoomItem";
 import ChatRoomsData from "../assets/dummy-data/ChatRooms";
+import { Auth } from "aws-amplify";
 
 export default function HomeScreen() {
+  const logOut = () => {
+    Auth.signOut();
+  };
+
   return (
     <View style={styles.page}>
       <FlatList
@@ -12,10 +17,23 @@ export default function HomeScreen() {
         renderItem={({ item }) => <ChatRoomItem chatRoom={item} />}
         showsVerticalScrollIndicator={false}
       />
+
+      <Pressable
+        onPress={logOut}
+        style={{
+          backgroundColor: "red",
+          height: 50,
+          margin: 10,
+          borderRadius: 25,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Text>Logout</Text>
+      </Pressable>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   page: {
     backgroundColor: "white",
